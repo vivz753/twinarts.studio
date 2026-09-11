dec8897577227de4d8c78c745fc8ca7153b51333
Shue Snyder <cyclingfuns@gmail.com>
2026-09-11 11:23:54 -0700

    Update projectId in sanity.cli.ts

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'lpnrji1s',
+    projectId: 'flmc67dk',
     dataset: 'production',
   },
   /**
