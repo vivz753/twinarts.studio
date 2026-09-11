faacda5cfb74075b4d30b86934df3741b507237c
Shue Snyder <cyclingfuns@gmail.com>
2026-09-11 11:37:02 -0700

    Update Sanity project ID in client configuration

--- web/src/sanity/client.ts
+++ web/src/sanity/client.ts
@@ -1,6 +1,6 @@
 import { createClient as makeClient } from "next-sanity"
 export const client = makeClient({
-  projectId: "lpnrji1s",
+  projectId: "flmc67dk",
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false, // important for ISR in Next.js, to revalidate cached data in case updates to Sanity are published
